340b048e4662d7a8eabd6de8134b765875d1f2c3
CalistusK <ekathens@hotmail.com>
2017-08-06 20:49:23 -0700

    Minor updates
    Stop trying to use fields that don't exist & prototype of rulings
    command.

--- Pylistus/mtgac.py
+++ Pylistus/mtgac.py
@@ -12,18 +12,21 @@ bot = commands.Bot(command_prefix='.')
 @bot.command()
 async def c(*, cardname: str):
 
-	card = {"name": cardname}
+	card = {"name": cardname, "orderBy": 'name'}
 	response = requests.get("https://api.magicthegathering.io/v1/cards", params=card)
 	cardinfo = json.loads(response.text)
 	carddata = cardinfo['cards'][0]
 
 	name = carddata['name'] + ' '
-	cost = carddata['manaCost'] + '\n'
+	if "manaCost" in carddata:
+		cost = carddata['manaCost'] + '\n'
+	else:
+		cost = '\n'
 	rarity = carddata['rarity'] + ' ('
 	fromset = carddata['set'] + ')\n'
 	cardtype = carddata['type'] + '\n'
 	cardtext = carddata['text']
-	if carddata['types'] == "Creature":
+	if "power" in carddata:
 		pt = '\n' + carddata['power'] + '/' + carddata['toughness']
 	else:
 		pt = ''
@@ -31,6 +34,31 @@ async def c(*, cardname: str):
 	match = name + cost + rarity + fromset + cardtype + cardtext + pt
 
 	await bot.say(match)
+
+# @bot.command()
+
+# There are no provisions for sending messages <2000 characters long
+# in the following code. Need to fix before re-enabling.
+
+# async def r(*, cardname: str):
+
+# 	card = {"name": cardname, "orderBy": 'name'}
+# 	response = requests.get("https://api.magicthegathering.io/v1/cards", params=card)
+# 	cardinfo = json.loads(response.text)
+# 	carddata = cardinfo['cards'][0]
+
+# 	if "rulings" in carddata:
+# 		rulecount = len(carddata['rulings'])
+# 		ruletar = carddata['rulings']
+# 		arules = ''
+
+# 		for rn in list(range(rulecount)):
+# 			arules = arules + ruletar[rn]['date'] + ' - ' + ruletar[rn]['text'] + '\n'
+
+# 	else:
+# 		arules = 'No rulings found for ' + carddata['name']
+
+# 	await bot.say(arules)
 
 config.read('config.ini')
 bot.run(config['DiscordAPI']['Key'])
