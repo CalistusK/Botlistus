56fb5f907464c3c77cd2cf199d4dbcde167646c5
CalistusK <ekathens@hotmail.com>
2017-08-09 22:39:28 -0700

    Multiplying like rabbits
    Switched old commands over to Scryfall and added new ones. Desperately
    need to clean this code up.

--- Pylistus/mtgac.py
+++ Pylistus/mtgac.py
@@ -13,6 +13,8 @@ bot = commands.Bot(command_prefix='.')
 async def mtg():
 	helplist = """ The following commands are available:\n
 	.c <cardname> - Search for the Oracle text of a card\n
+	.ce <cardname> - Search for the Oracle text of a card by exact name\n
+	.cf <cardname> - Search for the Oracle text of a card by approximate name\n
 	.cn <nickname> - Search for the Oracle text of a card by nickname\n 
 	.p <cardname> - Search for the price (USD) of a card\n
 	.ps <set> <cardname> - Search for the price (USD) of a card by set\n
@@ -24,26 +26,76 @@ async def mtg():
 @bot.command()
 async def c(*, cardname: str):
 
-	card = {"name": cardname, "orderBy": 'name'}
-	response = requests.get("https://api.magicthegathering.io/v1/cards", params=card)
+	card = {"q": cardname}
+	response = requests.get("https://api.scryfall.com/cards/search", params=card)
 	cardinfo = json.loads(response.text)
-	carddata = cardinfo['cards'][0]
+	carddata = cardinfo['data'][0]
 
 	name = carddata['name'] + ' '
-	if "manaCost" in carddata:
-		cost = carddata['manaCost'] + '\n'
+	if "mana_cost" in carddata:
+		cost = carddata['mana_cost'] + '\n'
 	else:
 		cost = '\n'
 	rarity = carddata['rarity'] + ' ('
 	fromset = carddata['set'] + ')\n'
-	cardtype = carddata['type'] + '\n'
-	cardtext = carddata['text']
+	cardtype = carddata['type_line'] + '\n'
+	cardtext = carddata['oracle_text']
 	if "power" in carddata:
 		pt = '\n' + carddata['power'] + '/' + carddata['toughness']
 	else:
 		pt = ''
 
-	match = name + cost + rarity + fromset + cardtype + cardtext + pt
+	match = name + cost + rarity.title() + fromset.upper() + cardtype.replace("â€”","—") + cardtext + pt
+
+	await bot.say(match)
+
+@bot.command()
+async def ce(*, cardname: str):
+
+	card = {"exact": cardname}
+	response = requests.get("https://api.scryfall.com/cards/named", params=card)
+	cardinfo = json.loads(response.text)
+
+	name = cardinfo['name'] + ' '
+	if "mana_cost" in cardinfo:
+		cost = cardinfo['mana_cost'] + '\n'
+	else:
+		cost = '\n'
+	rarity = cardinfo['rarity'] + ' ('
+	fromset = cardinfo['set'] + ')\n'
+	cardtype = cardinfo['type_line'] + '\n'
+	cardtext = cardinfo['oracle_text']
+	if "power" in cardinfo:
+		pt = '\n' + cardinfo['power'] + '/' + cardinfo['toughness']
+	else:
+		pt = ''
+
+	match = name + cost + rarity.title() + fromset.upper() + cardtype.replace("â€”","—") + cardtext + pt
+
+	await bot.say(match)
+
+@bot.command()
+async def cf(*, cardname: str):
+
+	card = {"fuzzy": cardname}
+	response = requests.get("https://api.scryfall.com/cards/named", params=card)
+	cardinfo = json.loads(response.text)
+
+	name = cardinfo['name'] + ' '
+	if "mana_cost" in cardinfo:
+		cost = cardinfo['mana_cost'] + '\n'
+	else:
+		cost = '\n'
+	rarity = cardinfo['rarity'] + ' ('
+	fromset = cardinfo['set'] + ')\n'
+	cardtype = cardinfo['type_line'] + '\n'
+	cardtext = cardinfo['oracle_text']
+	if "power" in cardinfo:
+		pt = '\n' + cardinfo['power'] + '/' + cardinfo['toughness']
+	else:
+		pt = ''
+
+	match = name + cost + rarity.title() + fromset.upper() + cardtype.replace("â€”","—") + cardtext + pt
 
 	await bot.say(match)
 
@@ -51,6 +103,32 @@ async def c(*, cardname: str):
 async def cn(*, cardname: str):
 
 	card = {"q": 'is:' + cardname}
+	response = requests.get("https://api.scryfall.com/cards/search", params=card)
+	cardinfo = json.loads(response.text)
+	carddata = cardinfo['data'][0]
+
+	name = carddata['name'] + ' '
+	if "mana_cost" in carddata:
+		cost = carddata['mana_cost'] + '\n'
+	else:
+		cost = '\n'
+	rarity = carddata['rarity'] + ' ('
+	fromset = carddata['set'] + ')\n'
+	cardtype = carddata['type_line'] + '\n'
+	cardtext = carddata['oracle_text']
+	if "power" in carddata:
+		pt = '\n' + carddata['power'] + '/' + carddata['toughness']
+	else:
+		pt = ''
+
+	match = name + cost + rarity.title() + fromset.upper() + cardtype.replace("â€”","—") + cardtext + pt
+
+	await bot.say(match)
+
+@bot.command()
+async def cs(setname: str, *, cardname: str):
+
+	card = {"q": 'e:' + setname + ' ' + cardname}
 	response = requests.get("https://api.scryfall.com/cards/search", params=card)
 	cardinfo = json.loads(response.text)
 	carddata = cardinfo['data'][0]
